26dbe89fea44cc0e08ce0a487683e419bcb5aff3
Om Patel <iamompatel@live.com>
2024-08-01 13:25:12 -0700

    8/1

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -13,7 +13,7 @@ searchInput.addEventListener("input", (e) =>{
     })
 })
 
-fetch("https://script.googleusercontent.com/macros/echo?user_content_key=Qg1pKelsP-GnOAUgD5T1oPnQ_MzPWXO-EuYgtCnvRQawEcr9RraHWy5Y4GvwfnRBrOQs68Fo2UvfJov24e3r-kMwmuT0IPlUm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHzDMuwdtJqnrRDr3A3fr68Pe0VCrStoQw5z0SlUsbIwWL3sRn5aG8rLm1JVau6cD2fYuUsd60DjfHQo2EfO8B46YNJZ_0SwQQ&lib=MGyzfp4jlAaTA2-OTV99yUiGR0zf_cVsI")
+fetch("https://script.googleusercontent.com/macros/echo?user_content_key=hRqrVxf3MnxcwihZt8wvwcySwjN2hyMAAZWkmgv8LuHkFj5flxAjcCxbZxfwT1dzFlAhdqTKqS0IMJa9DFQ1lcHNVVCxg4dDm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHzDMuwdtJqnrRDr3A3fr68Pe0VCrStoQw5z0SlUsbIwWL3sRn5aG8rLm1JVau6cD2fYuUsd60DjfHQo2EfO8B46YNJZ_0SwQQ&lib=MGyzfp4jlAaTA2-OTV99yUiGR0zf_cVsI")
     .then(res => res.json())
     .then(data => {
         values = Object.values(data);
@@ -24,8 +24,11 @@ fetch("https://script.googleusercontent.com/macros/echo?user_content_key=Qg1pKel
             const card = userCardTemplate.content.cloneNode(true).children[0]
             const header = card.querySelector("[data-header]")
             const body = card.querySelector("[data-body]")
+            const IMG = card.querySelector("[data-img]")
             header.textContent = user.Name
             body.textContent = user.Ranking
+             IMG.src = user.Images
+      
             userCardContainer.appendChild(card)
             return {Name: user.Name, Type: user.Ranking, element: card}
         })
