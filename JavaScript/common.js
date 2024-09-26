e1872e8b91bb23f95270c917354d637a6c66c813
Om Patel <iamompatel@live.com>
2024-09-25 21:42:56 -0700

    Review Section Done- CSS Needed

--- JavaScript/common.js
+++ JavaScript/common.js
@@ -2,5 +2,7 @@ const API_SIGN_URL = 'https://script.google.com/macros/s/AKfycbx-V6eCL-PQXDUlIHi
 
 const API_SCHOOL_URL =  'https://script.google.com/macros/s/AKfycbxT7QQjG0bmgQhDnV-bCh8J25ToXyjSdcOnoRDcOKFWvVrfYaZAoJJP05TZMXV8_PTE/exec';
 
+const API_REVIEW_URL = 'https://script.google.com/macros/s/AKfycbxxeoLL5ydwZc-GiyQr9OxKs-SngLqx6mYgJH-EHK45lU9e4b1wlfflftGwQLRh7W-dCA/exec';
+
 
 
